d1061ffca42062a2a1b79a4c8ba58eff78257993
hanoi0126 <ipocchi129@gmail.com>
2024-09-17 13:40:45 +0900

    implemente face detect

--- fastcomposer/evaluate_replication_templete.py
+++ fastcomposer/evaluate_replication_templete.py
@@ -1,22 +1,75 @@
 import cv2
 import hydra
+import numpy as np
+from facenet_pytorch import MTCNN
 from omegaconf import DictConfig
+from PIL import Image
+from torch import Tensor
 
 
-@hydra.main(config_path="../configs", config_name="eval_config")
+class FaceDetector:
+    def __init__(self):
+        self.mtcnn = MTCNN(margin=0, keep_all=True)
+
+    def get_face_deteil(self, image: Image):
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+
+        face_bboxes, probabilities = self.mtcnn.detect(image)
+        if face_bboxes is None or len(face_bboxes) == 0:
+            raise ValueError("顔が検出されませんでした。")
+
+        return face_bboxes, probabilities
+
+    def to_numpy(self, image: Tensor) -> np.ndarray:
+        image = (
+            image.squeeze(0).permute(1, 2, 0).cpu().numpy()
+        )  # (C, H, W) -> (H, W, C)
+        image = (image * 255).astype(np.uint8)  # [0, 1] -> [0, 255]
+
+        return image
+
+    def to_pil_image(self, image: Tensor) -> Image:
+        image = (
+            image.squeeze(0).permute(1, 2, 0).cpu().numpy()
+        )  # (C, H, W) -> (H, W, C)
+        image = (image * 255).astype(np.uint8)  # [0, 1] -> [0, 255]
+
+        return Image.fromarray(image)  # PIL.Image に変換
+
+
+@hydra.main(version_base=None, config_path="../configs", config_name="eval_config")
 def template_matching(cfg: DictConfig) -> None:
 
-    reference_image = cv2.imread(cfg.reference_image)
-    genearted_image = cv2.imread(cfg.generated_image)
-    w, h = reference_image.shape[::-1]
+    detector = FaceDetector()
+    reference_image = Image.open(cfg.reference_image)
+    face_bboxes, probabilities = detector.get_face_deteil(reference_image)
 
-    result = cv2.matchTemplate(genearted_image, reference_image, cv2.TM_CCOEFF_NORMED)
+    template_image = reference_image.crop(face_bboxes[0].tolist())
+    h, w = template_image.size
+    template_image = template_image.resize((h // 4, w // 4))
+
+    template_image.save(f"{cfg.output_dir}/detected_face.png")
+
+    template_image = cv2.imread(f"{cfg.output_dir}/detected_face.png")
+    generated_image = cv2.imread(cfg.generated_image)
+
+    if generated_image is None:
+        raise ValueError("生成画像が正しく読み込まれませんでした。")
+
+    h, w = template_image.shape[:2]
+
+    result = cv2.matchTemplate(generated_image, template_image, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print("Max value:", max_val, "Max location:", max_loc)
 
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv2.rectangle(genearted_image, top_left, bottom_right, 255, 2)
-    cv2.imwrite(f"{cfg.output_dir}/output_0.png", genearted_image)
+    cv2.rectangle(
+        generated_image, top_left, bottom_right, thickness=4, color=(0, 255, 255)
+    )
+
+    cv2.imwrite(f"{cfg.output_dir}/result.png", generated_image)
 
 
 if __name__ == "__main__":
