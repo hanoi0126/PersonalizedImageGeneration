7e1e8841bf22fd64f110de270fb19fc5d10f0747
hanoi0126 <ipocchi129@gmail.com>
2024-09-17 14:54:59 +0900

    implement feature matching

---
+++ fastcomposer/evaluate_replication_feature.py
@@ -0,0 +1,32 @@
+import cv2
+import hydra
+import numpy as np
+from facenet_pytorch import MTCNN
+from omegaconf import DictConfig
+from PIL import Image
+from torch import Tensor
+
+
+@hydra.main(version_base=None, config_path="../configs", config_name="eval_config")
+def feature_matching(cfg: DictConfig) -> None:
+
+    reference_image = cv2.imread(cfg.reference_image)
+    generated_image = cv2.imread(cfg.generated_image)
+
+    orb = cv2.ORB_create()
+    kp1, des1 = orb.detectAndCompute(reference_image, None)
+    kp2, des2 = orb.detectAndCompute(generated_image, None)
+
+    # BFMatcherによる特徴点のマッチング
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+
+    # マッチング結果の描画
+    matches = sorted(matches, key=lambda x: x.distance)
+    img_matches = cv2.drawMatches(reference_image, kp1, generated_image, kp2, matches[:10], None, flags=2)
+
+    cv2.imwrite(f"{cfg.output_dir}/result.png", img_matches)
+
+
+if __name__ == "__main__":
+    feature_matching()
