6554d679a6ae6aeeb247b9a1f47da5d605fa2d74
hanoi0126 <ipocchi129@gmail.com>
2024-09-24 22:45:35 +0900

    examine toy experiment

--- fastcomposer/evaluate_replication_vlm.py
+++ fastcomposer/evaluate_replication_vlm.py
@@ -18,7 +18,7 @@ SYSTEM_PROMPT = (
     "Do these images look like the person in the input image pasted on top?\n"
     "Please categorize each image into one of the following three categories:\n"
     "1. The person appears to be the same, but the image does not look pasted (natural differences in facial expression, orientation, etc.)\n"
-    "2. The person appears to be the same, but the image looks pasted or unnatural (identical facial expression, highly similar orientation, gestures are exactly the same, etc.)\n"
+    "2. The identical person is depicted, but only the face portion is in a paste-on format, showing an replication.\n"
     "3. The person does not appear to be the same\n"
     "Provide your reasoning for the categorization.\n"
     "Output the results in the following json format and do not output any string other than json:\n"
@@ -51,7 +51,7 @@ def generate_response_with_images(cfg: DictConfig) -> None:
         generated_image_paths = sorted(
             os.path.join(cfg.generated_image_dir, f)
             for f in os.listdir(cfg.generated_image_dir)
-            if f.endswith(".png") and not f.startswith("grid")
+            if (f.endswith(".png") or f.endswith(".jpg")) and not f.startswith("grid")
         )
     else:
         generated_image_paths = [cfg.generated_image]
@@ -65,42 +65,47 @@ def generate_response_with_images(cfg: DictConfig) -> None:
         ]
         example_url = encode_image(example_image)
         image_urls = [encode_image(image) for image in images]
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": SYSTEM_PROMPT},
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "image_url", "image_url": {"url": example_url}}
-                    ],
-                },
-                {
-                    "role": "assistant",
-                    "content": "Here is an example image. Prease refer to this image when answering the question.",
-                },
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "image_url", "image_url": {"url": image_url}}
-                        for image_url in image_urls
-                    ],
-                },
-            ],
-            max_tokens=300,
-        )
+        for i in range(cfg.request_for_image):
+            response = client.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "image_url", "image_url": {"url": example_url}}
+                        ],
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "Here is an example image. Prease refer to this image when answering the question.",
+                    },
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "image_url", "image_url": {"url": image_url}}
+                            for image_url in image_urls
+                        ],
+                    },
+                ],
+                max_tokens=300,
+            )
 
-        result = response.choices[0].message.content
-        print(result)
+            result = response.choices[0].message.content
+            print(result)
 
-        output_text += f"{generated_image_path}: {result}\n"
-        match = re.search(r"{(.|\s)*}", result.replace("'", '"'))
+            output_text += f"{generated_image_path}_{i:02}: {result}\n"
+            match = re.search(r"{(.|\s)*}", result.replace("'", '"'))
 
-        if match:
-            # JSONとして扱うために辞書に変換
-            json_data = json.loads(match.group())
+            if match:
+                # JSONとして扱うために辞書に変換
+                try:
+                    json_data = json.loads(match.group())
+                    output_dict[f"{generated_image_path}_{i:02}"] = json_data
+                except json.JSONDecodeError:
+                    print("JSON decode error")
+                    continue
 
-        output_dict[generated_image_path] = json_data
 
     with open(f"{cfg.output_dir}/result.txt", "w") as f:
         f.write(output_text)
